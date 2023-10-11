9d3d76d5a1f801a093a670f17ec728ef91ed66c5
Nils Kucza <nkucza@cit-ec.uni-bielefeld.de>
2023-10-11 15:49:53 +0200

    remove config value

--- launch/rtface_emo_launch.py
+++ launch/rtface_emo_launch.py
@@ -9,14 +9,12 @@ def generate_launch_description():
         "topic_sub", default_value=TextSubstitution(text="face_id"))
     topic_pub_launch_arg = DeclareLaunchArgument(
         "topic_pub", default_value=TextSubstitution(text="emotion_id"))
-    eng_rebuild_launch_arg = DeclareLaunchArgument(
-        "eng_rebuild", default_value=TextSubstitution(text="True"))
     eng_save_launch_arg = DeclareLaunchArgument(
         "eng_save", default_value=TextSubstitution(text="True"))
     eng_onnx_path_launch_arg = DeclareLaunchArgument(
         "eng_onnx_path", default_value=TextSubstitution(text="/opt/dev/DL_Models/face_rec/model_emotion/enet_b2_7.onnx"))
     eng_save_path_launch_arg = DeclareLaunchArgument(
-        "eng_save_path", default_value=TextSubstitution(text="/opt/dev/DL_Models/face_rec/model_emotion/emotion.rt"))
+        "eng_save_path", default_value=TextSubstitution(text="/opt/dev/engines/emotion.rt"))
     eng_use_dla_launch_arg = DeclareLaunchArgument(
         "eng_use_dla", default_value=TextSubstitution(text="-1"))
     eng_fp_16_launch_arg = DeclareLaunchArgument(
@@ -25,7 +23,6 @@ def generate_launch_description():
     return LaunchDescription([
         topic_sub_launch_arg,
         topic_pub_launch_arg,
-        eng_rebuild_launch_arg,
         eng_save_launch_arg,
         eng_onnx_path_launch_arg,
         eng_save_path_launch_arg,
@@ -41,7 +38,6 @@ def generate_launch_description():
             parameters=[
                 {"topic_sub": LaunchConfiguration('topic_sub'),
                  "topic_pub": LaunchConfiguration('topic_pub'),
-                 "eng_rebuild": LaunchConfiguration('eng_rebuild'),
                  "eng_save": LaunchConfiguration('eng_save'),
                  "eng_onnx_path": LaunchConfiguration('eng_onnx_path'),
                  "eng_save_path": LaunchConfiguration('eng_save_path'),
